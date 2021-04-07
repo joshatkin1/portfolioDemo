003adaf97d0f22091460b43c86b2e81b3d4a5816
Josh Atkin <joshatkin@hotmail.co.uk>
2021-04-07 15:06:34 +0100

    test

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,4 @@ Route::get('/app', [App\Http\Controllers\AppController::class, 'appView'])->name
 Route::get('/profile', [App\Http\Controllers\ProfileController::class, 'profileView'])->name('profile');
 
 
-Route::get('/GITTEST', [App\Http\Controllers\ProfileController::class, 'profileView'])->name('profile');
+Route::get('/sss', [App\Http\Controllers\ProfileController::class, 'profileView'])->name('profile');
